6dafaf22c48382c85a595a3d25980c19cbe5b34b
António Dias <antoniod@ipatimup.up>
2018-10-24 11:13:38 +0100

    Historical commit before removing anchor option and start always using center coordinates

--- OpenSeadragonHTMLelements.js
+++ OpenSeadragonHTMLelements.js
@@ -25,7 +25,8 @@
     // {
     //   id: <string>,
     //   element: <HTMLelement>,
-    //   rect: <OpenSeadragon.rect> (in image coordinates)
+    //   rect: <OpenSeadragon.Rect> (in image coordinates)
+    //   (optional) anchor: "center"(default) || "origin"
     // }
     this.elements = [];
 
@@ -33,6 +34,7 @@
     this.viewer.addHandler("open", function() {repositionElements(self.elements)})
     this.viewer.addHandler("animation", function () {repositionElements(self.elements)})
     this.viewer.addHandler("rotate", function() {repositionElements(self.elements)})
+    this.viewer.addHandler("", function(ev) {console.log(ev)})
   }
     // ----------
   $.hElements.prototype = {
@@ -44,6 +46,8 @@
     },
     addElement: function(e) {
       if (validateElement(e)) {
+        e.element.style.width = "100%"
+        e.element.style.height = "100%"
         let wrapperDiv = document.createElement("div");
         wrapperDiv.style.position = "absolute";
         wrapperDiv.appendChild(e.element);
@@ -75,9 +79,37 @@
     goToElementLocation: function(id) {
       const e = this.getElementById(id)
       if (e !== null) {
-        this.viewer.viewport.fitBoundsWithConstraints(
-          this.viewer.viewport.imageToViewportRectangle(e.rect)
-        )
+        const vpRect = this.viewer.viewport.imageToViewportRectangle(e.rect)
+        // const point = flipCheck(i.position, viewerInfo)
+        const pos = viewer.viewport.imageToViewportCoordinates(e.rect.x, e.rect.y)
+        if (e.anchor === "center") {
+          this.viewer.viewport.fitBoundsWithConstraints(new OpenSeadragon.Rect(
+            pos.x - vpRect.width / 2,
+            pos.y - vpRect.height / 2,
+            vpRect.width,
+            vpRect.height
+          ))
+
+        } else if (e.anchor === "origin") {
+          // this.viewer.viewport.fitBoundsWithConstraints(vpRect)
+          // this.viewer.viewport.panTo(new OpenSeadragon.Point(
+          //   pos.x + vpRect.width / 2,
+          //   pos.y + vpRect.height / 2
+          // ))
+          // const point = flipCheck(i.position, viewerInfo)
+          // this.viewer.viewport.fitBoundsWithConstraints(new OpenSeadragon.Rect(
+          //   pos.x,
+          //   pos.y,
+          //   vpRect.width,
+          //   vpRect.height
+          // ))
+          this.viewer.viewport.fitBoundsWithConstraints(new OpenSeadragon.Rect(
+            pos.x - vpRect.width,
+            pos.y - vpRect.height,
+            vpRect.width,
+            vpRect.height
+          ))
+        }
       }
     }
   }
@@ -88,6 +120,10 @@
 
 const validateElement = function(e) {
   let isValid = true
+  if (!("anchor" in e)) {
+    e.anchor = "center"
+  }
+
   let errors = []
   if (!("id" in e)) {
     isValid = false
@@ -103,18 +139,34 @@ const validateElement = function(e) {
   }
   if (errors.length !== 0) {
     console.log("Missing properties", errors.join(", "), ". Element was not added: ", e)
+  } else if (!e.anchor.match("center|origin")) {
+    console.log("Wrong anchor value. Use 'center' or 'origin'. Element was not added: ", e)
   }
+
   return isValid
 }
 
 const repositionElements = function(es) {
   for (let e of es) {
-    const newRect = viewer.viewport.viewportToViewerElementRectangle(
-      viewer.viewport.imageToViewportRectangle(e.rect)
-    )
-    e.element.style.left = newRect.x + "px"
-    e.element.style.top = newRect.y + "px"
-    e.element.style.width = newRect.width + "px"
-    e.element.style.height = newRect.height + "px"
+    repositionElement(e)
   }
+}
+
+const repositionElement = function(e) {
+  const newRect = viewer.viewport.viewportToViewerElementRectangle(
+    viewer.viewport.imageToViewportRectangle(e.rect)
+  )
+  // const point = flipCheck(i.position, viewerInfo)
+  const pos = viewer.viewport.viewportToViewerElementCoordinates(
+    viewer.viewport.imageToViewportCoordinates(e.rect.x, e.rect.y)
+  )
+  if (e.anchor === "center") {
+    e.element.style.left = pos.x - newRect.width / 2 + "px"
+    e.element.style.top = pos.y - newRect.height / 2 + "px"
+  } else if (e.anchor === "origin") {
+    e.element.style.left = pos.x + "px"
+    e.element.style.top = pos.y + "px"
+  }
+  e.element.style.width = newRect.width + "px"
+  e.element.style.height = newRect.height + "px"
 }
